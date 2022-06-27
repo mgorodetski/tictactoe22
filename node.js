f70d442b955cf43bceeb9ef12d7d58580827eba6
mgorodetski <87032361+mgorodetski@users.noreply.github.com>
2022-06-27 22:57:12 +0300

    port fixed

--- node.js
+++ node.js
@@ -2,7 +2,7 @@ const express = require('express');
 // const cors = require('cors')
 const path = require('path');
 const app = express();
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
 // app.use(cors()); no cors
 app.use((req, res, next) => {
